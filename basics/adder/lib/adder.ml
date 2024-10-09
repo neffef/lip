a3e4e5c55727a43868a3d2bdac61d67df01c8ded
Docker <docker@example.com>
2024-10-09 14:01:05 +0100

    Addertest.ml risolto

--- basics/adder/lib/adder.ml
+++ basics/adder/lib/adder.ml
@@ -1,4 +1,5 @@
 (* val addlist : int list -> int *)
 (* addlist l adds the element of the list of integers l *)
 
-let addlist l = 0 (* replace 0 with actual code *)
+let addlist l = List.fold_left(+) 0 l;;
+
