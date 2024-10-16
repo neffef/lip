223f0ea5636745f6933d10be93ac47682437baef
Docker <docker@example.com>
2024-10-16 14:40:29 +0100

    Risolto esercizio

--- contextfree/lib/exercises.ml
+++ contextfree/lib/exercises.ml
@@ -16,15 +16,56 @@ let todo : grammar =
 
 
 (* #### Exercise 1, easy (zero_n_one_n) *)
-let zero_n_one_n : grammar = todo
+let zero_n_one_n : grammar = 
+  {
+    symbols = [ S ];
+    terminals = [ '0'; '1' ];
+    productions =
+      [
+        S --> "0S1";
+        S --> "";
+      ];
+    start = S;
+  }
 
 
 (* #### Exercise 2, easy (palindromes) *)
-let palindromes : grammar = todo
+let palindromes : grammar = 
+  {
+    symbols = [ S ];
+    terminals = [ '0'; '1' ];
+    productions =
+      [
+        S --> "0S0";
+        S --> "1S1";
+        S --> "0";
+        S --> "1";
+        S --> ""
+      ];
+    start = S;
+  }
 
 
 (* #### Exercise 3, medium (balanced_parentheses)*)
-let balanced_parentheses : grammar = todo
+let balanced_parentheses : grammar = 
+  {
+    symbols = [ S ];
+    terminals = [ '('; ')' ; '{' ; '}'; '['; ']'];
+    productions =
+      [
+        S --> "(S)";
+        S --> "[S]";
+        S --> "{S}";
+        S --> "()S";
+        S --> "[]S";
+        S --> "{}S";
+        S --> "S()";
+        S --> "S[]";
+        S --> "S{}";
+        S --> "";
+      ];
+    start = S;
+  }
 
 
 (* #### Exercise 4, hard (same_amount)
@@ -33,4 +74,21 @@ let balanced_parentheses : grammar = todo
    Hint 2: think of the language of words where the number of 0s is one greater
    than the number of 1s and viceversa, then combine them.
 *)
-let same_amount : grammar = todo
+let same_amount : grammar = 
+  {
+    symbols = [ S ];
+    terminals = [ '0'; '1' ];
+    productions =
+      [
+        S --> "0S1";
+        S --> "01S";
+        S --> "S01";
+        S --> "10S";
+        S --> "1S0";
+        S --> "S10";
+        S --> "0S0";
+        S --> "1S1";
+        S --> ""
+      ];
+    start = S;
+  }
